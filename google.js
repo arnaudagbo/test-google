3346b31838ee2f9f27aa0b8a11f82d02e320d092
Arnaud AGBO <agbo.arnaud@yahoo.fr>
2017-10-05 22:17:34 +0200

    Ajout du bouton afficher les éléments centraux

---
+++ google.js
@@ -0,0 +1,11 @@
+let clik = document.getElementById("clik");
+let ensemble = document.getElementById("ensemble");
+
+clik.onclick = function() {
+    this.style.visibility = "hidden";
+    ensemble.style.visibility = "visible";
+}
+
+setTimeout(function() {
+    alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+}, 10000);
